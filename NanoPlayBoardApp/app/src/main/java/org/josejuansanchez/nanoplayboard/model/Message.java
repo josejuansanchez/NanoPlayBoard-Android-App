194f54150d6945b94a72e0cab415e70fda126a17
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-06-24 03:06:59 +0200

    Added model package

---
+++ NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/model/Message.java
@@ -0,0 +1,27 @@
+package org.josejuansanchez.nanoplayboard.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by josejuansanchez on 24/6/16.
+ */
+public class Message implements Serializable {
+    private int potentiometer;
+    private int ldr;
+
+    public int getPotentiometer() {
+        return potentiometer;
+    }
+
+    public void setPotentiometer(int potentiometer) {
+        this.potentiometer = potentiometer;
+    }
+
+    public int getLdr() {
+        return ldr;
+    }
+
+    public void setLdr(int ldr) {
+        this.ldr = ldr;
+    }
+}
