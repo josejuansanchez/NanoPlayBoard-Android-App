41fb8b72fdbabdca6ee8bec915c96fac0c8c028d
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-10-03 12:26:10 +0200

    Added protocol constants

---
+++ NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/constants/ProtocolConstants.java
@@ -0,0 +1,27 @@
+package org.josejuansanchez.nanoplayboard.constants;
+
+/**
+ * Created by josejuansanchez on 03/10/16.
+ */
+
+public class ProtocolConstants {
+    public static final int ID_POTENTIOMETER_READ = 10;
+    public static final int ID_POTENTIOMETER_SCALE_TO = 11;
+
+    public static final int ID_LDR_READ = 20;
+    public static final int ID_LDR_SCALE_TO = 21;
+
+    public static final int ID_RGB_ON = 30;
+    public static final int ID_RGB_OFF = 31;
+    public static final int ID_RGB_TOGGLE = 32;
+    public static final int ID_RGB_SET_COLOR = 33;
+    public static final int ID_RGB_SET_INTENSITY = 34;
+
+    public static final int ID_BUZZER_PLAY_TONE = 40;
+    public static final int ID_BUZZER_STOP_TONE = 41;
+
+    public static final int ID_LEDMATRIX_PRINT_CHAR = 50;
+    public static final int ID_LEDMATRIX_PRINT_PATTERN = 51;
+    public static final int ID_LEDMATRIX_PRINT_STRING = 52;
+    public static final int ID_LEDMATRIX_PRINT_IN_LAND = 53;
+}
