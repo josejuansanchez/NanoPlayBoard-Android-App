41ccb509fd87bf2b1f836d19eba2c96009806b72
José Juan Sánchez <josejuan.sanchez@gmail.com>
2016-10-06 13:05:58 +0200

    Fixed #28

--- NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/activities/LDRActivity.java
+++ NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/activities/LDRActivity.java
@@ -1,6 +1,7 @@
 package org.josejuansanchez.nanoplayboard.activities;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 import org.josejuansanchez.nanoplayboard.R;
 import org.josejuansanchez.nanoplayboard.constants.ProtocolConstants;
@@ -8,7 +9,7 @@ import org.josejuansanchez.nanoplayboard.models.NanoPlayBoardMessage;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.OnCheckedChanged;
 import tr.xip.markview.MarkView;
 
 public class LDRActivity extends NanoPlayBoardActivity {
@@ -24,9 +25,22 @@ public class LDRActivity extends NanoPlayBoardActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_start)
-    void onClick() {
+    @OnCheckedChanged(R.id.togglebutton_start_stop)
+    void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            startReadingLdr();
+        } else {
+            stopReadingLdr();
+        }
+    }
+
+    void startReadingLdr() {
         NanoPlayBoardMessage message = new NanoPlayBoardMessage(ProtocolConstants.ID_LDR_READ);
+        sendJsonMessage(message);
+    }
+
+    void stopReadingLdr() {
+        NanoPlayBoardMessage message = new NanoPlayBoardMessage(ProtocolConstants.ID_LDR_STOP);
         sendJsonMessage(message);
     }
 
--- NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/activities/PotentiometerActivity.java
+++ NanoPlayBoardApp/app/src/main/java/org/josejuansanchez/nanoplayboard/activities/PotentiometerActivity.java
@@ -1,6 +1,7 @@
 package org.josejuansanchez.nanoplayboard.activities;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 import org.josejuansanchez.nanoplayboard.R;
 import org.josejuansanchez.nanoplayboard.constants.ProtocolConstants;
@@ -8,7 +9,7 @@ import org.josejuansanchez.nanoplayboard.models.NanoPlayBoardMessage;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.OnCheckedChanged;
 import tr.xip.markview.MarkView;
 
 public class PotentiometerActivity extends NanoPlayBoardActivity {
@@ -24,9 +25,22 @@ public class PotentiometerActivity extends NanoPlayBoardActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.button_start)
-    void onClick() {
+    @OnCheckedChanged(R.id.togglebutton_start_stop)
+    void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            startReadingPotentiometer();
+        } else {
+            stopReadingPotentiometer();
+        }
+    }
+
+    void startReadingPotentiometer() {
         NanoPlayBoardMessage message = new NanoPlayBoardMessage(ProtocolConstants.ID_POTENTIOMETER_READ);
+        sendJsonMessage(message);
+    }
+
+    void stopReadingPotentiometer() {
+        NanoPlayBoardMessage message = new NanoPlayBoardMessage(ProtocolConstants.ID_POTENTIOMETER_STOP);
         sendJsonMessage(message);
     }
 
